4801ab0964fcc7b3dc823890739b96fde372a5a7
Toby Shearman <toby@marginem.io>
2026-04-15 03:13:02 -0400

    improving zsh history configuration

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -6,6 +6,14 @@ in
   programs.zsh = {
     enable = true;
     shellAliases = shellAliases.common // shellAliases.zsh;
+    history = {
+      size = 50000;
+      save = 50000;
+      ignoreDups = true;
+      ignoreSpace = true;
+      extended = true;
+      share = true;
+    };
     initContent = ''
       export PATH="/opt/homebrew/bin:$PATH"
       eval "$(/opt/homebrew/bin/brew shellenv)"
